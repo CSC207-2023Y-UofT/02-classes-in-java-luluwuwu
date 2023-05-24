777fb185c025cf362e2e2d5c52d7c43def2a2121
luluwuwu <133042713+luluwuwu@users.noreply.github.com>
2023-05-24 17:52:01 -0400

    github exercise 2

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -76,10 +76,13 @@ public class Silly implements Comparable<Silly>{
      * TODO (Task 1): Create another constructor that takes in TWO parameters,
      *       both of which are strings.
      *       Afterwards, set this.name to the concatenation of both
-     *       strings (e.g. this.name = [first string] + [second string]).
+     *       strings (e.g. this.name = [first string] + [second string])
      *       Make sure you document this method!
      */
-
+    public Silly(String first, String sec) {
+        this.name = first + sec;
+    }
+    // Takes two String variables, joins them, and returns new String name.
 
 
 
@@ -116,7 +119,7 @@ public class Silly implements Comparable<Silly>{
         y.countStatic();
         x.countStatic();
         x.countStatic();
-        int[] expected_values = {};
+        int[] expected_values = {0,1,2,3};
 
         System.out.println("The countStatic calls will return " + Arrays.toString(expected_values));
     }
@@ -133,6 +136,7 @@ public class Silly implements Comparable<Silly>{
      */
     @Override
     public String toString(){
+        return this.name;
         // TODO (Task 3): Implement the body of this method!
     }
 
@@ -158,6 +162,7 @@ public class Silly implements Comparable<Silly>{
         }
 
         Silly other = (Silly) o; // To access .name of o, we need to cast it.
+        return this.name.equals(other.name);
 
         // Hint: to compare strings, we need to use .equals()
         //       e.g. s1.equals(s2)
@@ -194,6 +199,13 @@ public class Silly implements Comparable<Silly>{
          *                You can get the length of a string by using the
          *                .length() method.
          */
+        if (this.toString().length() > other.toString().length()){
+            return 1;
+        } else if (this.toString().length() < other.toString().length()) {
+            return -1;
+        }else {
+            return 0;
+        }
     }
 
     /*
